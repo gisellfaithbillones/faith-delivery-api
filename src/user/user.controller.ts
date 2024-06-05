6376c079f4217410776af374959ffeba2525ab1a
jaylordibe <jaylordibe@gmail.com>
2024-06-05 18:05:54 +0800

    Initial project

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,8 +5,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {
-    }
+    constructor(private readonly userService: UserService) {}
 
     @Post()
     create(@Body() createUserDto: CreateUserDto) {
